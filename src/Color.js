e344ca03b158d13102f488ff388c68a928eb7065
Aaron Brant <72094000+KortesmakiAB@users.noreply.github.com>
2021-06-01 23:47:02 -0400

    Fixed <Redirect> bug in Color.js. Gussied up ColorAdd form.

--- src/Color.js
+++ src/Color.js
@@ -1,22 +1,17 @@
-import { useEffect } from 'react';
-import { Link, useParams } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
 import './Color.css';
 
 const Color = ({ getColor }) => {
     const { color } = useParams();
     
-    const { color: colorName, hex } = getColor(color);
+    const colorObj = getColor(color);
     
-    useEffect(() => {
-        const body = document.getElementById("body");
-        body.style.backgroundColor = hex || colorName;
-        return () => {
-            body.style.backgroundColor = 'white';
-        }
-    }, [hex, colorName]);
+    if (!colorObj) return <Redirect to='/colors'></Redirect>
 
+    const { color: colorName, hex } = colorObj
+    
     return (
-        <div className="Color">
+        <div className="Color" style={{ backgroundColor: hex || colorName}}>
             <h1>THIS IS { color.toUpperCase() } </h1>
             <h1>ISN'T IT BEAUTIFUL!</h1>
             <Link className="Color-a" to="/colors"><h1>GO BACK</h1></Link>
--- src/ColorAdd.js
+++ src/ColorAdd.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
+import './ColorAdd.css';
 
 const ColorAdd = ({ addColor }) => {
     
@@ -25,18 +26,22 @@ const ColorAdd = ({ addColor }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label to="color-name">Color Name</label>
-                <input id="color-name" type="text" value={colorName} onChange={handleNameChange}></input>
-            </div>
-            <div>
-                <label to="color-value">Color Value</label>
-                <input id="color-value" type="color" value={colorValue} onChange={handleColorChange}></input>
-            </div>
-
-            <button>Add this color</button>
-        </form>
+        <div className="ColorAdd">
+            <form className="ColorAdd-form" onSubmit={handleSubmit}>
+                <div>
+                    <label to="color-name">Color Name</label>
+                    <input id="color-name" type="text" value={colorName} onChange={handleNameChange}></input>
+                </div>
+                <div>
+                    <label to="color-value">Color Value</label>
+                    <input id="color-value" type="color" value={colorValue} onChange={handleColorChange}></input>
+                </div>
+                <div>
+                    <button>Add this color</button>
+                </div>
+                
+            </form>
+        </div>
     );
 };
 
